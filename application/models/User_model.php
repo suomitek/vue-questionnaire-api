fef841b03e6ab05a9c32424df438bc8ba6ef34b3
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-09-18 15:48:07 +0800

    3. add data transform detail

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,18 +1,15 @@
 <?php
 
-class User_model extends CI_Model
-{
+class User_model extends CI_Model {
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         // Your own constructor code
         $this->load->database();
     }
 
     // 获取用户
-    public function get_users()
-    {
+    public function get_users() {
         // 搜索功能
         $keyword = $this->input->post_get('keyword', TRUE);
         $value = $this->input->post_get('value', TRUE);
@@ -83,8 +80,7 @@ class User_model extends CI_Model
     }
 
     // 更新用户
-    public function update_user()
-    {
+    public function update_user() {
         $u_id = json_decode($this->input->raw_input_stream, true)['u_id'];
         $update_data = array(
 //            'u_major' => json_decode($this->input->raw_input_stream, true)['u_major'],
@@ -105,8 +101,7 @@ class User_model extends CI_Model
     }
 
     // 批量上传,新增用户
-    public function add_user($data)
-    {
+    public function add_user($data) {
         // 如果工号冲突，则不添加此用户
         $is_exist = $this->db->get_where('users', array('u_number' => $data[3]));
         if ($is_exist->num_rows() > 0) {
@@ -131,8 +126,7 @@ class User_model extends CI_Model
     }
 
     // 清空所有用户
-    public function clear_user()
-    {
+    public function clear_user() {
         // 清空用户的同时，删除所有与其关联的数据
         $this->db->empty_table("users");
         $this->db->empty_table("submit_log");
@@ -148,8 +142,7 @@ class User_model extends CI_Model
     }
 
     // 删除用户
-    public function del_user()
-    {
+    public function del_user() {
         $user_id = json_decode($this->input->raw_input_stream, true)['u_id'];
 
         // 删除多表中的数据
@@ -184,8 +177,7 @@ class User_model extends CI_Model
     }
 
     // 获取用户ID
-    public function get_user_id()
-    {
+    public function get_user_id() {
         $this->config->load('settings', TRUE);
         $name = json_decode($this->input->raw_input_stream, true)['name'];
         $identity = json_decode($this->input->raw_input_stream, true)['identity'];
@@ -221,8 +213,7 @@ class User_model extends CI_Model
     }
 
     // 获取班级列表
-    public function get_class_list()
-    {
+    public function get_class_list() {
         $query = $this->db->query('SELECT u_class FROM users GROUP BY u_class');
         $result = $query->result_array();
         return array('err' => 0, "data" => $result);
